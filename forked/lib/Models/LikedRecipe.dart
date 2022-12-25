5f49b08f1ddf10c5df0bd7459765f6d16cda6952
Amjad-SD <117661420+Amjad-SD@users.noreply.github.com>
2022-12-25 19:25:29 +0300

    forgot password

--- forked/lib/Models/LikedRecipe.dart
+++
@@ -1,5 +0,0 @@
-import 'package:forked/Models/Recipie.dart';
-
-class LikedRecipe extends Recipes {
-  int? number_Of_Likes;
-}
--- forked/lib/Models/SavedRecipes.dart
+++
@@ -1,5 +0,0 @@
-import 'package:forked/Models/Recipie.dart';
-
-class SavedRecipes extends Recipes {
-  bool? isSaved;
-}
