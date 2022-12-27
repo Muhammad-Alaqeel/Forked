3e2e0d0f6a4060538a59a4efe6f0092a5cfeb27c
Muhammed Alaqeel <muhammed_alaqeel@hotmail.com>
2022-12-27 14:31:53 +0300

    exploring functionality completed

--- forked/lib/Controllers/ListsOfHomeController.dart
+++ forked/lib/Controllers/ListsOfHomeController.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:forked/Components/CardRecipeCard.dart';
 import 'package:forked/Components/userDisplay.dart';
+import 'package:forked/Models/forkedRecipe.dart';
+import 'package:forked/Services/FireStoreRequests/RiecipeRequests.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+import '../Models/originalRecipie.dart';
+import '../Services/FireStoreRequests/forkedRecipeRequests.dart';
 
 class ListsOfHomeController extends GetxController {
   List<Widget> daliyInspirationList = [];
   List<Widget> mostPopularList = [];
   List<Widget> followingList = [];
   List<Widget> exploreList = [];
-  
+ 
 
   @override
-  void onInit() {
+  void onInit()async {
     // TODO: implement onInit
     super.onInit();
     for (var i = 0; i < 5; i++) {
       addInDaliyInspirationList();
       addInMostPopularList();
-      addInFollowingList();
-      addInExploreList();
+     await addInFollowingList();
     }
+exploreList= await exploring(exploreList);
+
+    update();
   }
 
 
@@ -69,15 +76,86 @@ class ListsOfHomeController extends GetxController {
     ));
   }
 
-  addInExploreList() {
-    exploreList.add(Container(
-      height: Get.height * .3,
-      width: Get.width * .4,
-      child: SmallCardRecipeCard(
-        recipeName: "pancake",
-        userName: "",
-        ImageFunct: () {},
+
+
+
+  Future<List<Widget>> exploring(List<Widget> shuffeled)async{
+
+List<originalRecipe> explorrer= await readAllOriginalRecipies();
+List<forkedRecipe> forkedExplorer= await readAllforkedlRecipies();
+
+explorrer.shuffle();
+
+forkedExplorer.shuffle();
+
+
+
+    for (var i=0; i<10; i++) {
+     
+  
+
+      if(i%2==0&& i<explorrer.length){
+Get.snackbar("ortiginal", explorrer[i].title.toString());
+     shuffeled.add(   Padding(
+     padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+      child: ConstrainedBox(
+      constraints: BoxConstraints(
+      //   minWidth: Get.width*.5,
+        // minHeight: Get.height*.45,
+      //   maxHeight: Get.height*.46,
+      //  maxWidth: Get.width*.51,
+
+      ),
+        child: SmallCardRecipeCard(
+          recipeName: explorrer[i].title!,
+          recipeImage: "images/6.jpg",
+          userName:
+              explorrer[i].username,
+          ImageFunct: () {},
+          likeFunct: (){},
+          accontFunct: (){},
+        ),
       ),
     ));
+      }
+
+
+      if(i%2==1&& i<forkedExplorer.length){
+        Get.snackbar("forked", forkedExplorer[i].title.toString());
+
+          exploreList.add(   Padding(
+     padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+      child: ConstrainedBox(
+      constraints: BoxConstraints(
+      //   minWidth: Get.width*.5,
+        // minHeight: Get.height*.45,
+      //   maxHeight: Get.height*.46,
+      //  maxWidth: Get.width*.51,
+
+      ),
+        child: SmallCardRecipeCard(
+          recipeName: forkedExplorer[i].title!,
+          recipeImage: "images/6.jpg",
+          userName:
+              forkedExplorer[i].username,
+          ImageFunct: () {},
+          likeFunct: (){},
+          accontFunct: (){},
+        ),
+      ),
+    ));
+      }
+
+
+
+
+
+
+
+    };
+    shuffeled.shuffle();
+
+  return shuffeled;
   }
 }
+
